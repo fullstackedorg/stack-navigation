50a8dbb0fb921d49a3e8957b09f8332ca5ed2281
cplepage <lepage.charlesphilippe@gmail.com>
2025-04-03 09:37:57 -0400

    fix and move to repo install

--- index.ts
+++ index.ts
@@ -1,267 +1,83 @@
-globalThis.back = function () {
-    return StackNavigation.singleton?.back();
+import StackNavigation from "./stack-navigation";
+
+const stackNavigation = new StackNavigation();
+
+const randColor = () => {
+    let color = "";
+    while (color?.length !== 6) {
+        color = Math.floor(Math.random() * 16777215).toString(16);
+    }
+    return "#" + color;
 };
 
-export default class StackNavigation {
-    static singleton: StackNavigation;
-    private views: {
-        element: HTMLElement;
-        scrollHack: HTMLElement;
-        inner: HTMLElement;
-        onDestroy?: () => void;
-    }[] = [];
+const main = document.createElement("main");
+main.id = "grid";
+const elementCount = 30;
 
-    lock = false;
+const inputContainer = document.createElement("div");
+inputContainer.innerHTML = `<input />`
+main.append(inputContainer);
 
-    leftInit = 50;
-    velocityThreshold = 1;
-
-    behindViewOffset = 0.3;
-
-    constructor() {
-        StackNavigation.singleton = this;
-
-        window.addEventListener("touchstart", this.onStart.bind(this));
-        window.addEventListener("touchmove", this.onMove.bind(this));
-        window.addEventListener("touchend", this.onEnd.bind(this));
-
-        document.body.style.margin = "0px";
-        document.body.style.padding = "0px";
-        document.body.style.overflow = "hidden";
-
-        this.adjustHeightToAvailableViewPort();
-    }
-
-    private adjustHeightToAvailableViewPort() {
-        let lastHeight = 0;
-        const checkHeight = () => {
-            const currentHeight = window.visualViewport.height;
-
-            // soft keyboard is probably shown
-            if (currentHeight < document.body.clientHeight) {
-                window.scrollTo(0, 0);
-                this.views.at(-1).inner.style.height = "calc(100% + 2px)";
-                this.views.at(-1).scrollHack.scrollTo(0, 1);
-            } else if(this.views?.at(-1)?.inner) {
-                this.views.at(-1).inner.style.height = "100%";
-            }
-
-            if (currentHeight !== lastHeight) {
-                this.views.forEach(
-                    (v) => (v.element.style.height = currentHeight + "px"),
-                );
-            }
-
-            lastHeight = currentHeight;
-
-            window.requestAnimationFrame(checkHeight);
-        };
-        checkHeight();
-    }
-
-    private drag: {
-        id: number;
-        start: {
-            x: number;
-            timestamp: number;
-        };
-        end: {
-            x: number;
-            timestamp: number;
-        };
-    } = null;
-    onStart(e: TouchEvent) {
-        if (this.views.length <= 1 || e.touches.length > 1 || this.lock) return;
-
-        const start = e.touches.item(0).clientX;
-
-        if (start > this.leftInit) return;
-
-        this.drag = {
-            id: e.touches.item(0).identifier,
-            start: {
-                x: start,
-                timestamp: Date.now(),
-            },
-            end: {
-                x: start,
-                timestamp: Date.now(),
-            },
-        };
-        const currentView = this.views.at(-1);
-        currentView.element.style.transition = "none";
-        currentView.element.style.transform = `translate3d(0px, 0px, 0px)`;
-        currentView.element.classList.add("navigating");
-        (currentView.element.children[0] as HTMLElement).style.overflow =
-            "hidden";
-
-        this.views.at(-2).element.style.transition = "none";
-    }
-    onMove(e: TouchEvent) {
-        if (this.drag === null) return;
-
-        let draggingTouch: Touch;
-        for (let i = 0; i < e.touches.length; i++) {
-            const touch = e.touches.item(i);
-            if (touch.identifier === this.drag.id) {
-                draggingTouch = touch;
-                break;
-            }
-        }
-
-        if (!draggingTouch) {
-            this.onEnd(e);
-            return;
-        }
-
-        this.drag.end = {
-            x: draggingTouch.clientX,
-            timestamp: Date.now(),
-        };
-        let deltaX = this.drag.end.x - this.drag.start.x;
-
-        this.views.at(-1).element.style.transform =
-            `translate3d(${deltaX > 0 ? deltaX : 0}px, 0px, 0px)`;
-
-        const percentX = deltaX / window.innerWidth;
-        const deltaBehindViewPercent = this.behindViewOffset * percentX;
-        const translationX =
-            (this.behindViewOffset - deltaBehindViewPercent) * 100;
-        this.views.at(-2).element.style.transform =
-            `translate3d(-${translationX > 0 ? translationX : 0}%, 0px, 0px)`;
-    }
-    onEnd(e: TouchEvent) {
-        if (this.drag === null) return;
-
-        let stillDragging = false;
-        for (let i = 0; i < e.touches.length; i++) {
-            const touch = e.touches.item(i);
-            if (touch.identifier === this.drag.id) {
-                stillDragging = true;
-                break;
-            }
-        }
-
-        if (stillDragging) return;
-
-        const deltaTime = this.drag.end.timestamp - this.drag.start.timestamp;
-        const deltaX = this.drag.end.x - this.drag.start.x;
-
-        const velocity = deltaX / deltaTime;
-
-        if (
-            velocity > this.velocityThreshold ||
-            this.drag.end.x > window.innerWidth * 0.5
-        ) {
-            this.back();
-        } else {
-            const currentView = this.views.at(-1);
-            currentView.element.style.transition = "0.3s transform";
-            currentView.element.style.transform = `translate3d(0px, 0px, 0px)`;
-            currentView.element.classList.remove("navigating");
-            (currentView.element.children[0] as HTMLElement).style.overflow =
-                "auto";
-
-            const behindView = this.views.at(-2);
-            behindView.element.style.transition = "0.3s transform";
-            behindView.element.style.transform = `translate3d(-${this.behindViewOffset * 100}%, 0px, 0px)`;
-        }
-
-        this.drag = null;
-    }
-
-    navigate(e: HTMLElement, options?: Partial<ViewOptions>) {
-        const view = document.createElement("div");
-        view.style.cssText = `
-            position: fixed;
-            height: 100%;
-            width: 100%;
-            top: 0;
-            left: 0;
-            overflow: hidden;`;
-
-        // DEPRECATING string options for bgColor (2024-10-09)
-        if (typeof options === "string") {
-            options = {
-                bgColor: options,
-            };
-        }
-
-        if (options?.bgColor) {
-            view.style.backgroundColor = options.bgColor;
-        }
-        view.style.transition = `0.3s transform`;
-        view.style.transform = `translate3d(100%, 0px, 0px)`;
-
-        // source: https://codepen.io/josephshambrook/pen/WvwyqX
-        const scrollHack = document.createElement("div");
-        scrollHack.style.cssText = `
-            overflow: auto;
-            height: 100%;
-            width: 100%;
-            -webkit-overflow-scrolling: touch;
-            overscroll-behavior: none;
-            position: relative;`;
-
-        const inner = document.createElement("div");
-        inner.style.cssText = `
-            height: 100%;
-            width: 100%;
-            position: absolute;
-            top: 0;
-            left: 0;
-            overflow: auto;`;
-
-        inner.append(e);
-        scrollHack.append(inner);
-        view.append(scrollHack);
-
-        this.views.forEach((v) => {
-            v.element.style.pointerEvents = "none";
-            v.element.style.transition = `0.3s 1ms transform`;
-            v.element.style.transform = `translate3d(-${this.behindViewOffset * 100}%, 0px, 0px)`;
-        });
-
-        this.views.push({
-            element: view,
-            scrollHack,
-            inner,
-            ...(options || {}),
-        });
-        document.body.append(view);
-
-        setTimeout(() => {
-            if (this.views.at(-1).element === view)
-                view.style.transform = `translate3d(0%, 0px, 0px)`;
-        }, 1);
-    }
-
-    // return if did go back
-    back() {
-        if (this.views.length <= 1) {
-            return false;
-        }
-
-        const lastView = this.views.pop();
-        lastView.onDestroy?.();
-        lastView.element.style.transition = "0.3s transform";
-        lastView.element.style.transform = `translate3d(${window.innerWidth}px, 0px, 0px)`;
-        setTimeout(() => lastView.element.remove(), 400);
-
-        const currentView = this.views.at(-1);
-        currentView.element.style.transition = "0.3s transform";
-        currentView.element.style.pointerEvents = "all";
-        currentView.element.style.transform = `translate3d(0%, 0px, 0px)`;
-
-        return true;
-    }
-
-    reset() {
-        while (this.back()) {}
-    }
+for (let i = 0; i < elementCount; i++) {
+    const element = document.createElement("div");
+    const color = randColor();
+    element.style.backgroundColor = color;
+    main.append(element);
+    element.addEventListener("click", () => navigateToColorView(color));
 }
 
-type ViewOptions = {
-    bgColor: string;
-    onDestroy: () => void;
-};
+stackNavigation.navigate(main);
+
+function getColorByBgColor(bgColor: string) {
+    if (!bgColor) {
+        return "";
+    }
+    return parseInt(bgColor.replace("#", ""), 16) > 0xffffff / 2
+        ? "#000"
+        : "#fff";
+}
+
+function navigateToColorView(bgColor: string) {
+    const view = document.createElement("div");
+    view.classList.add("color-view");
+    view.style.color = getColorByBgColor(bgColor);
+    view.innerText = bgColor;
+
+    const back = document.createElement("div");
+    back.classList.add("back-btn");
+    back.addEventListener("click", () => stackNavigation.back());
+    view.append(back);
+
+    const input = document.createElement("input");
+    view.append(input);
+
+    stackNavigation.navigate(view, {
+        bgColor,
+        onDestroy: () => {
+            Toast(`Removed view with bg ${bgColor}`);
+        },
+    });
+}
+
+const toastDuration = 2000;
+function Toast(text: string) {
+    const container = document.createElement("div");
+    container.classList.add("toast");
+
+    const inner = document.createElement("div");
+    inner.innerText = text;
+    container.append(inner);
+
+    const destroy = () => container.remove();
+    const hide = () => {
+        container.classList.remove("show");
+        setTimeout(destroy, 400);
+    };
+    const show = () => {
+        container.classList.add("show");
+        setTimeout(hide, toastDuration);
+    };
+
+    setTimeout(show, 1);
+    document.body.append(container);
+}
---
+++ stack-navigation.ts
@@ -0,0 +1,270 @@
+globalThis.back = function () {
+    return StackNavigation.singleton?.back();
+};
+
+export default class StackNavigation {
+    static singleton: StackNavigation;
+    private views: {
+        element: HTMLElement;
+        scrollHack: HTMLElement;
+        inner: HTMLElement;
+        onDestroy?: () => void;
+    }[] = [];
+
+    lock = false;
+
+    leftInit = 50;
+    velocityThreshold = 1;
+
+    behindViewOffset = 0.3;
+
+    constructor() {
+        StackNavigation.singleton = this;
+
+        window.addEventListener("touchstart", this.onStart.bind(this));
+        window.addEventListener("touchmove", this.onMove.bind(this));
+        window.addEventListener("touchend", this.onEnd.bind(this));
+
+        document.body.style.margin = "0px";
+        document.body.style.padding = "0px";
+        document.body.style.overflow = "hidden";
+
+        this.adjustHeightToAvailableViewPort();
+    }
+
+    private adjustHeightToAvailableViewPort() {
+        let lastHeight = 0;
+        const checkHeight = () => {
+            const currentView = this.views.at(-1);
+            if (currentView) {
+                const currentHeight = window.visualViewport.height;
+
+                // soft keyboard is probably shown
+                if (currentHeight < document.body.clientHeight) {
+                    window.scrollTo(0, 0);
+                    currentView.inner.style.height = "calc(100% + 2px)";
+                    currentView.scrollHack.scrollTo(0, 1);
+                } else if (this.views?.at(-1)?.inner) {
+                    currentView.inner.style.height = "100%";
+                }
+
+                if (currentHeight !== lastHeight) {
+                    this.views.forEach(
+                        (v) => (v.element.style.height = currentHeight + "px"),
+                    );
+                }
+
+                lastHeight = currentHeight;
+            }
+
+            window.requestAnimationFrame(checkHeight);
+        };
+        checkHeight();
+    }
+
+    private drag: {
+        id: number;
+        start: {
+            x: number;
+            timestamp: number;
+        };
+        end: {
+            x: number;
+            timestamp: number;
+        };
+    } = null;
+    onStart(e: TouchEvent) {
+        if (this.views.length <= 1 || e.touches.length > 1 || this.lock) return;
+
+        const start = e.touches.item(0).clientX;
+
+        if (start > this.leftInit) return;
+
+        this.drag = {
+            id: e.touches.item(0).identifier,
+            start: {
+                x: start,
+                timestamp: Date.now(),
+            },
+            end: {
+                x: start,
+                timestamp: Date.now(),
+            },
+        };
+        const currentView = this.views.at(-1);
+        currentView.element.style.transition = "none";
+        currentView.element.style.transform = `translate3d(0px, 0px, 0px)`;
+        currentView.element.classList.add("navigating");
+        (currentView.element.children[0] as HTMLElement).style.overflow =
+            "hidden";
+
+        this.views.at(-2).element.style.transition = "none";
+    }
+    onMove(e: TouchEvent) {
+        if (this.drag === null) return;
+
+        let draggingTouch: Touch;
+        for (let i = 0; i < e.touches.length; i++) {
+            const touch = e.touches.item(i);
+            if (touch.identifier === this.drag.id) {
+                draggingTouch = touch;
+                break;
+            }
+        }
+
+        if (!draggingTouch) {
+            this.onEnd(e);
+            return;
+        }
+
+        this.drag.end = {
+            x: draggingTouch.clientX,
+            timestamp: Date.now(),
+        };
+        let deltaX = this.drag.end.x - this.drag.start.x;
+
+        this.views.at(-1).element.style.transform =
+            `translate3d(${deltaX > 0 ? deltaX : 0}px, 0px, 0px)`;
+
+        const percentX = deltaX / window.innerWidth;
+        const deltaBehindViewPercent = this.behindViewOffset * percentX;
+        const translationX =
+            (this.behindViewOffset - deltaBehindViewPercent) * 100;
+        this.views.at(-2).element.style.transform =
+            `translate3d(-${translationX > 0 ? translationX : 0}%, 0px, 0px)`;
+    }
+    onEnd(e: TouchEvent) {
+        if (this.drag === null) return;
+
+        let stillDragging = false;
+        for (let i = 0; i < e.touches.length; i++) {
+            const touch = e.touches.item(i);
+            if (touch.identifier === this.drag.id) {
+                stillDragging = true;
+                break;
+            }
+        }
+
+        if (stillDragging) return;
+
+        const deltaTime = this.drag.end.timestamp - this.drag.start.timestamp;
+        const deltaX = this.drag.end.x - this.drag.start.x;
+
+        const velocity = deltaX / deltaTime;
+
+        if (
+            velocity > this.velocityThreshold ||
+            this.drag.end.x > window.innerWidth * 0.5
+        ) {
+            this.back();
+        } else {
+            const currentView = this.views.at(-1);
+            currentView.element.style.transition = "0.3s transform";
+            currentView.element.style.transform = `translate3d(0px, 0px, 0px)`;
+            currentView.element.classList.remove("navigating");
+            (currentView.element.children[0] as HTMLElement).style.overflow =
+                "auto";
+
+            const behindView = this.views.at(-2);
+            behindView.element.style.transition = "0.3s transform";
+            behindView.element.style.transform = `translate3d(-${this.behindViewOffset * 100}%, 0px, 0px)`;
+        }
+
+        this.drag = null;
+    }
+
+    navigate(e: HTMLElement, options?: Partial<ViewOptions>) {
+        const view = document.createElement("div");
+        view.style.cssText = `
+            position: fixed;
+            height: 100%;
+            width: 100%;
+            top: 0;
+            left: 0;
+            overflow: hidden;`;
+
+        // DEPRECATING string options for bgColor (2024-10-09)
+        if (typeof options === "string") {
+            options = {
+                bgColor: options,
+            };
+        }
+
+        if (options?.bgColor) {
+            view.style.backgroundColor = options.bgColor;
+        }
+        view.style.transition = `0.3s transform`;
+        view.style.transform = `translate3d(100%, 0px, 0px)`;
+
+        // source: https://codepen.io/josephshambrook/pen/WvwyqX
+        const scrollHack = document.createElement("div");
+        scrollHack.style.cssText = `
+            overflow: auto;
+            height: 100%;
+            width: 100%;
+            -webkit-overflow-scrolling: touch;
+            overscroll-behavior: none;
+            position: relative;`;
+
+        const inner = document.createElement("div");
+        inner.style.cssText = `
+            height: 100%;
+            width: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            overflow: auto;`;
+
+        inner.append(e);
+        scrollHack.append(inner);
+        view.append(scrollHack);
+
+        this.views.forEach((v) => {
+            v.element.style.pointerEvents = "none";
+            v.element.style.transition = `0.3s 1ms transform`;
+            v.element.style.transform = `translate3d(-${this.behindViewOffset * 100}%, 0px, 0px)`;
+        });
+
+        this.views.push({
+            element: view,
+            scrollHack,
+            inner,
+            ...(options || {}),
+        });
+        document.body.append(view);
+
+        setTimeout(() => {
+            if (this.views.at(-1).element === view)
+                view.style.transform = `translate3d(0%, 0px, 0px)`;
+        }, 1);
+    }
+
+    // return if did go back
+    back() {
+        if (this.views.length <= 1) {
+            return false;
+        }
+
+        const lastView = this.views.pop();
+        lastView.onDestroy?.();
+        lastView.element.style.transition = "0.3s transform";
+        lastView.element.style.transform = `translate3d(${window.innerWidth}px, 0px, 0px)`;
+        setTimeout(() => lastView.element.remove(), 400);
+
+        const currentView = this.views.at(-1);
+        currentView.element.style.transition = "0.3s transform";
+        currentView.element.style.pointerEvents = "all";
+        currentView.element.style.transform = `translate3d(0%, 0px, 0px)`;
+
+        return true;
+    }
+
+    reset() {
+        while (this.back()) {}
+    }
+}
+
+type ViewOptions = {
+    bgColor: string;
+    onDestroy: () => void;
+};
